225552e6680685e62f9696a9d20a8105e81f7d34
bonitr02 <rianne.bonitto@gmail.com>
2023-11-21 19:27:51 +0000

    updated part2

--- part2/flask1/app.py
+++ part2/flask1/app.py
@@ -4,9 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    mathstuff = 7 + 8
-    output = f'Flask App 1 - Math output {mathstuff}'
+    WBC = 11000
+    Neutrophils = 30
+    Bands = 3
+
+    calculatedOutcome = ((Neutrophils + Bands)/ 100) * WBC
+    output = f'Flask App 1 - ANC Calculator Results: {calculatedOutcome}'
     return output
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=5002
+        )
--- part2/flask2/app.py
+++ part2/flask2/app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask App 2 !!!! \n This is a completly new flask application. \n Pretty cool! \n'
+
+    serum_albumin = 4.4
+    serum_calcium = 9
+
+    corrected_calcium = serum_calcium + 0.8 * (4 - serum_albumin)
+
+    output = f'Hello from Flask App 2 \n Corrected Calcium calculator: {corrected_calcium}'
+    return output
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=5001
+        )
